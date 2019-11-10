dfecede8407eb87a139b7e0be17aaa6793d878c9
Varun <varun@lollypop.design>
2019-11-10 16:38:57 +0530

    gitignore and build pushing

---
+++ build/precache-manifest.0fe071c52dffe59f43c63ba7a6ce891e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e6325761cd4aed25ece402068633df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ee1af1562e15ddc1751",
+    "url": "/static/css/main.915e758b.chunk.css"
+  },
+  {
+    "revision": "d09720d139a59d5e737b",
+    "url": "/static/js/2.56459eed.chunk.js"
+  },
+  {
+    "revision": "2ee1af1562e15ddc1751",
+    "url": "/static/js/main.37202f27.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  }
+]);
